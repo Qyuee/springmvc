0934a68b51ff1c1d4d75bc4685ff45dfb9d2bea9
Qyuee <lee33398@naver.com>
2021-06-14 23:55:17 +0900

    06/14

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -5,8 +5,7 @@ import hello.itemservice.domain.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -33,7 +32,90 @@ public class BasicItemController {
         return "basic/items";
     }
 
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    /**
+     * Form을 통해서 요청 데이터가 전달 됨
+     * - Content-Type: application/x-www-form-urlencoded
+     * - itemName=123&price=123&quantity=123
+     */
+    //@PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam int quantity,
+                       Model model) {
+        //itemRepository.save(item);
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    // @ModelAttribute를 사용한 버전
+    // @ModelAttribute를 사용 할 때, 이름을 지정해놓으면 model객체에 item까지 모두 넣어준다.
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
+        itemRepository.save(item);
+        // model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute의 name 속성을 지우면 해당 어노테이션이 붙은 class의 앞글자르 소문자로 변경하여 model에 바인딩한다.
+     * ex)
+     *  - @ModelAttribute Item item
+     *  - model.addAttribute("item", item);
+     *
+     *  - @ModelAttribute HelloData item
+     *  - model.addAttribute("helloData", item);
+     */
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute 생략
+     * - String, Integer 등의 타입은 @RequestParam이 적용
+     * - 우리가 생성한 임의의 타입은 @ModelAttribute가 적용된다.
+     */
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
     // 테스트용 데이터 삽입
+    // TODO: 2021-06-14 - 빈 생명주기 학습 필요
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemsA", 10000, 10));
